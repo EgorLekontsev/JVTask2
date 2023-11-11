139576e7f838db74fbf522c6a650b40c091e31a8
Timeless <89999195+EgorLekontsev@users.noreply.github.com>
2023-11-11 18:05:03 +0300

    Update Main.java

--- src/packet/Main.java
+++ src/packet/Main.java
@@ -1,11 +1,11 @@
 package packet;
 public class Main {
-	private static int x = 2;
+	private static int x = 2; //Объявляем и инициализируем наши 2 целочисленные переменные
 	private static int y = 14;
-	public static void main(String[] args) {
-        System.out.println("Before: x="+x+", y="+y);
-        Swap();
-        System.out.println("After: x="+x+", y="+ y);
+	public static void main(String[] args) { //Смотрим результат до и после
+        System.out.println("Before: x="+x+", y="+y); //2 и 14
+        Swap(); //Вызываем метод, для изменения переменных посредством математических операций
+        System.out.println("After: x="+x+", y="+ y); //14 и 2
 	}
 	public static void Swap(){
         x = x - y;
